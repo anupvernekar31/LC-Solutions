d70e64e02b0dbb0fd44fd3dbe37126fa71861936
anupvernekar31 <43994265+anupvernekar31@users.noreply.github.com>
2025-08-23 23:59:54 +0530

    Time: 5 ms (30.73%), Space: 45.4 MB (33.37%) - LeetHub

---
+++ 0120-triangle/0120-triangle.java
@@ -0,0 +1,28 @@
+class Solution {
+    // MEMOIZED SOLUTION GIVES TLE NOT SURE WHY ???
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int dp[][] = new int[n][n];
+
+        // Initialize the bottom row of dp with the values from the bottom row of the triangle
+        for (int j = 0; j < n; j++) {
+            dp[n - 1][j] = triangle.get(n-1).get(j);
+        }
+
+        // Starting from the second to last row, calculate the minimum path sum for each element
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i; j >= 0; j--) {
+                // Calculate the two possible paths: moving down or moving diagonally
+                int down = triangle.get(i).get(j) + dp[i + 1][j];
+                int diagonal = triangle.get(i).get(j) + dp[i + 1][j + 1];
+
+                // Store the minimum of the two paths in dp
+                dp[i][j] = Math.min(down, diagonal);
+            }
+        }
+
+        // The result is stored at the top of dp array
+        return dp[0][0];
+        
+    }
+}
