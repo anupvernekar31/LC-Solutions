04ecf08ece240cc7cbd6a9815c6ab3c82985a666
anupvernekar31 <43994265+anupvernekar31@users.noreply.github.com>
2025-09-29 16:50:25 +0530

    Time: 47 ms (79.3%), Space: 47.5 MB (68.41%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.java
@@ -5,28 +5,25 @@ class Solution {
 
     public int subarrayWithAtMostK(int[] nums, int k) {
         int n = nums.length;
-        int[] freq = new int[n + 1]; // Array to track frequency of elements
-        int count = 0; // Number of distinct elements
-        int ans = 0; // Result
+        int left=0, right = 0;
+        int ans =0;
+        HashMap<Integer, Integer> hm = new HashMap<>();
 
-        int i = 0;
-        for (int j = 0; j < n; j++) {
-            if (freq[nums[j]] == 0) {
-                count++;
-            }
-            freq[nums[j]]++;
-
+        while(right < n) {
+            hm.put(nums[right], hm.getOrDefault(nums[right], 0) +1);
+            
             // If count exceeds k, move the left pointer i to reduce the window size
-            while (count > k) {
-                freq[nums[i]]--;
-                if (freq[nums[i]] == 0) {
-                    count--;
+            while (hm.size() > k) {
+                hm.put(nums[left], hm.get(nums[left]) - 1);
+                if (hm.get(nums[left]) == 0) {
+                    hm.remove(nums[left]);
                 }
-                i++;
+                left++;
             }
 
             // For each valid window, update the answer
-            ans += j - i + 1;
+            ans += right - left + 1;
+            right++;
         }
 
         return ans;
