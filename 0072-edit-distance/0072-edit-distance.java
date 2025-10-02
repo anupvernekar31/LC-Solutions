4b3bb9db28ca979a62737c1475a1c6671bbc692a
anupvernekar31 <43994265+anupvernekar31@users.noreply.github.com>
2025-10-02 12:09:59 +0530

    Time: 5 ms (59.25%), Space: 45 MB (64.65%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -53,20 +53,6 @@ class Solution {
 // TABULATION SOLUTION
 // ===========================
 class Solution {
-
-    public int f(int i, int j, String word1, String word2, int dp[][]){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
-
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-
-        if(word1.charAt(i) == word2.charAt(j)){
-            return f(i-1,j-1,word1, word2,dp);
-        }
-        return 1+Math.min(f(i-1,j,word1, word2,dp), Math.min(f(i,j-1,word1, word2, dp),f(i-1,j-1,word1, word2, dp)));
-    }
     public int minDistance(String word1, String word2) {
         int n= word1.length();
         int m = word2.length();
